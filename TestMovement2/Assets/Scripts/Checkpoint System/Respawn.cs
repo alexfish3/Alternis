a2f5630af65e06e73fd95424a9a8486525a28f69
whosy999 <fosterthepeoples98@gmail.com>
2022-03-22 20:12:52 -0400

    Removed Event Handler Added Animation to Respawn
    Makes it a lot easier to directly tie the animation play to the respawn script itself so when the animation finishes the player respawns no animation right now considering their is no PostFX installed on our current build but the animation for it will be purely visual for now.

--- TestMovement2/Assets/Scripts/Checkpoint System/Respawn.cs
+++ TestMovement2/Assets/Scripts/Checkpoint System/Respawn.cs
@@ -8,6 +8,8 @@ public class Respawn : MonoBehaviour
     [SerializeField] private Transform respawner;
     [SerializeField] private GameObject player;
     [SerializeField] private float maxFallHeight = -15;
+    [SerializeField] private Animation anime;
+    [SerializeField] private float timer = 0;
     public bool useFallHeight;
 
     private void CurCheckpoint()
@@ -18,10 +20,48 @@ public class Respawn : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Death") && other.gameObject.layer == 7 && player.GetComponent<WorldSwap>().lightWorld)
         {
+            timer = 0;
+            anime.Play();
+            //The Animation must always be bigger than the timer as the timer is when the animation and player stops for respawns so their should be a little bit of leg room at the end of the animation.
+            while (anime.isPlaying)
+            {
+                //This is the timer for the intervals it's just adding the interval's until it reaches a certain point during the animation.
+                //This is the exact time between 2 frame calls in float added to whatever the current timer number is
+                //In order for us to figure out exactly what the perfect timer is just work with these numbers considering this will be the most consistent timing for the animation
+                Debug.Log("The Timer for death is currently:" + timer);
+                timer += Time.deltaTime;
+                Debug.Log("The Timer for death after addition is:" + timer);
+                if (timer > 3000)
+                {
+                    anime.Stop();
+                    DoRespawn();
+                }
+
+            }
+            //This is temporary because their is no animation;
             DoRespawn();
         }
         if (other.gameObject.tag == "Death")
         {
+            timer = 0;
+            anime.Play();
+            //The Animation must always be bigger than the timer as the timer is when the animation and player stops for respawns so their should be a little bit of leg room at the end of the animation.
+            while (anime.isPlaying)
+            {
+                //This is the timer for the intervals it's just adding the interval's until it reaches a certain value
+                //This is the exact time between 2 frame calls in float added to whatever the current timer number is
+                //In order for us to figure out exactly what the perfect timer is just work with these numbers considering this will be the most consistent timing for the animation
+                Debug.Log("The Timer for death is currently:" + timer);
+                timer += Time.deltaTime;
+                Debug.Log("The Timer for death after addition is:" + timer);
+                if (timer > 3000)
+                {
+                    anime.Stop();
+                    DoRespawn();
+                    anime.Rewind();
+                }
+            }
+            //This is temporary because their is no animation;
             DoRespawn();
         }
     }
@@ -36,7 +76,8 @@ public class Respawn : MonoBehaviour
     // Respawn
     public void DoRespawn()
     {
-        StartCoroutine(WaitRespawn());
+        CurCheckpoint();
+        player.transform.position = respawner.position;
     }
 
     public void loseOxygenRespawn()
@@ -58,14 +99,4 @@ public class Respawn : MonoBehaviour
         if (useFallHeight) didYouFall();
     }
 
-    IEnumerator WaitRespawn()
-    {
-        Time.timeScale = 0;
-        yield return new WaitForSecondsRealtime(1);
-        Time.timeScale = 1;
-        player.GetComponent<WorldSwap>().respawn();
-        Debug.LogError("Respawning");
-        CurCheckpoint();
-        player.transform.position = respawner.position;
-    }
 }
